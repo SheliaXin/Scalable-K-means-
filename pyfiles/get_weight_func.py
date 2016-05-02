7a43a07be33acced24616f95b05830a25c8d7f5d
Shelia Xin <xinxu.199301@gmail.com>
2016-05-01 22:34:29 -0400

    clean repo

--- pyfiles/get_weight_func.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-def get_weight(dist,centroids):
-    min_dist = np.zeros(dist.shape)
-    min_dist[range(dist.shape[0]), np.argmin(dist, axis=1)] = 1
-    count = np.array([np.count_nonzero(min_dist[:, i]) for i in range(centroids.shape[0])])
-    return count/np.sum(count)
